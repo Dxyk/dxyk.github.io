ec41bea6b3915cd57989aa2d3d70eedbe6e52baf
Xiangyu Kong <danielkong1997@gmail.com>
2025-09-22 10:59:15 -0400

    Add convergence and SPP to viewer

---
+++ unlisted/interactive_viewer/scenes/Convergence-B000/data.js
@@ -0,0 +1,136 @@
+const data =
+{
+    "imageBoxes": [
+        {
+            "title": "Prim1_SS1",
+            "elements": [
+                {
+                    "title": "Images",
+                    "elements": [
+                        {
+                            "title": "Reference",
+                            "version": "-",
+                            "image": "Prim1_SS1/Reference.png"
+                        },
+                        {
+                            "title": "Input",
+                            "version": "-",
+                            "image": "Prim1_SS1/Input.png"
+                        },
+                        {
+                            "title": "Avg_US",
+                            "version": "-",
+                            "image": "Prim1_SS1/Avg_US.png"
+                        },
+                        {
+                            "title": "Max_US",
+                            "version": "-",
+                            "image": "Prim1_SS1/Max_US.png"
+                        }
+                    ]
+                },
+                {
+                    "title": "NEG_POS SMAPE",
+                    "elements": [
+                        {
+                            "title": "Reference",
+                            "version": "-",
+                            "image": "Prim1_SS1/Reference-NEG_POS SMAPE.png"
+                        },
+                        {
+                            "title": "Input",
+                            "version": "-",
+                            "image": "Prim1_SS1/Input-NEG_POS SMAPE.png"
+                        },
+                        {
+                            "title": "Avg_US",
+                            "version": "-",
+                            "image": "Prim1_SS1/Avg_US-NEG_POS SMAPE.png"
+                        },
+                        {
+                            "title": "Max_US",
+                            "version": "-",
+                            "image": "Prim1_SS1/Max_US-NEG_POS SMAPE.png"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "title": "Prim16_SS16",
+            "elements": [
+                {
+                    "title": "Images",
+                    "elements": [
+                        {
+                            "title": "Reference",
+                            "version": "-",
+                            "image": "Prim16_SS16/Reference.png"
+                        },
+                        {
+                            "title": "Input",
+                            "version": "-",
+                            "image": "Prim16_SS16/Input.png"
+                        },
+                        {
+                            "title": "Avg_US",
+                            "version": "-",
+                            "image": "Prim16_SS16/Avg_US.png"
+                        },
+                        {
+                            "title": "Max_US",
+                            "version": "-",
+                            "image": "Prim16_SS16/Max_US.png"
+                        }
+                    ]
+                },
+                {
+                    "title": "NEG_POS SMAPE",
+                    "elements": [
+                        {
+                            "title": "Reference",
+                            "version": "-",
+                            "image": "Prim16_SS16/Reference-NEG_POS SMAPE.png"
+                        },
+                        {
+                            "title": "Input",
+                            "version": "-",
+                            "image": "Prim16_SS16/Input-NEG_POS SMAPE.png"
+                        },
+                        {
+                            "title": "Avg_US",
+                            "version": "-",
+                            "image": "Prim16_SS16/Avg_US-NEG_POS SMAPE.png"
+                        },
+                        {
+                            "title": "Max_US",
+                            "version": "-",
+                            "image": "Prim16_SS16/Max_US-NEG_POS SMAPE.png"
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    "stats": [
+        {
+            "title": "Stats",
+            "labels": [
+                "Prim1_SS1/Avg_US",
+                "Prim1_SS1/Max_US",
+                "Prim16_SS16/Avg_US",
+                "Prim16_SS16/Max_US"
+            ],
+            "series": [
+                {
+                    "label": "NEG_POS SMAPE",
+                    "data": [],
+                    "track": {
+                        "x": [],
+                        "y": []
+                    }
+                }
+            ]
+        }
+    ]
+}
