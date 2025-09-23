50b0522becc955726f40790b777e584e25ee132d
Xiangyu Kong <danielkong1997@gmail.com>
2025-09-22 21:36:50 -0400

    Add multiview blind test results

---
+++ unlisted/interactive_viewer/scenes/MultiView 3D Blind Test-B000/data.js
@@ -0,0 +1,79 @@
+const data =
+{
+    "imageBoxes": [
+        {
+            "title": "MultiView 3D Blind Test",
+            "elements": [
+                {
+                    "title": "Images",
+                    "elements": [
+                        {
+                            "title": "Reference",
+                            "version": "-",
+                            "image": "MultiView 3D Blind Test/Reference.png"
+                        },
+                        {
+                            "title": "Input",
+                            "version": "-",
+                            "image": "MultiView 3D Blind Test/Input.png"
+                        },
+                        {
+                            "title": "Prim1_SS1",
+                            "version": "-",
+                            "image": "MultiView 3D Blind Test/Prim1_SS1.png"
+                        },
+                        {
+                            "title": "Prim16_SS16",
+                            "version": "-",
+                            "image": "MultiView 3D Blind Test/Prim16_SS16.png"
+                        }
+                    ]
+                },
+                {
+                    "title": "NEG_POS SMAPE",
+                    "elements": [
+                        {
+                            "title": "Reference",
+                            "version": "-",
+                            "image": "MultiView 3D Blind Test/Reference-NEG_POS SMAPE.png"
+                        },
+                        {
+                            "title": "Input",
+                            "version": "-",
+                            "image": "MultiView 3D Blind Test/Input-NEG_POS SMAPE.png"
+                        },
+                        {
+                            "title": "Prim1_SS1",
+                            "version": "-",
+                            "image": "MultiView 3D Blind Test/Prim1_SS1-NEG_POS SMAPE.png"
+                        },
+                        {
+                            "title": "Prim16_SS16",
+                            "version": "-",
+                            "image": "MultiView 3D Blind Test/Prim16_SS16-NEG_POS SMAPE.png"
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    "stats": [
+        {
+            "title": "Stats",
+            "labels": [
+                "MultiView 3D Blind Test/Prim1_SS1",
+                "MultiView 3D Blind Test/Prim16_SS16"
+            ],
+            "series": [
+                {
+                    "label": "NEG_POS SMAPE",
+                    "data": [],
+                    "track": {
+                        "x": [],
+                        "y": []
+                    }
+                }
+            ]
+        }
+    ]
+}
